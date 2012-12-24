f5b1b532e0d76eb101b16f7af2de1958c9f70ba0
kpolaiah <kpolaiah@heresy.in>
2012-12-24 11:18:39 +0530

    manage circles changes in signed users.

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,9 @@ Relayfan::Application.routes.draw do
   namespace :signed do
     resources :users, :except => [:new, :create, :edit] do
      collection do
-      get "manage", "account_form", "edit_detail", "hide_wall_detail", "delete", "set_order", "destroy_image", "edit_category", "delete","block","unblock", "connection_types"
-      post "add_walls", "add_category_title"
-      put "add_walls", "add_photos", "add_category_title"
+      get "manage", "account_form", "edit_detail", "hide_wall_detail", "delete", "set_order", "destroy_image", "edit_category", "delete","block","unblock", "connection_types", "edit_circles", "hide_circle"
+      post "add_walls", "add_category_title", "add_circles_title"
+      put "add_walls", "add_photos", "add_category_title", "add_circles_title"
      end
     end
     resources :feeds do 
