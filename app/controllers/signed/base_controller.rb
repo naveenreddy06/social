342cce412b5fab4e6c53fa1683712b8b71c5242e
kpsrinivas <kpsrinivas@heresy.in>
2012-12-26 11:22:33 +0530

    request url for sign in.

--- app/controllers/signed/base_controller.rb
+++ app/controllers/signed/base_controller.rb
@@ -2,6 +2,7 @@ class Signed::BaseController < ApplicationController
   
   def check_authentication
     if !current_user
+      session[:coming_from] = request.url
       redirect_to root_path
     end
   end
