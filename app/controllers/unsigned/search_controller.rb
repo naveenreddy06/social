eb8902d3edf435c43c3a8f69bda80e47962d1a51
nallupradeepreddy <pradeep@heresy.in>
2012-12-31 19:32:42 +0530

    fixes.

--- app/controllers/unsigned/search_controller.rb
+++ app/controllers/unsigned/search_controller.rb
@@ -1,9 +1,13 @@
 class Unsigned::SearchController < Unsigned::BaseController
-  
+
   before_filter :set_flashes_to_null, :check_authentication
-  
+
   def index
-    @results = params[:model_type].constantize.search params
+  	unless params[:keyword].blank?
+      @results = params[:model_type].constantize.search params
+    else
+  	  @results = []
+    end
   end
-  
+
 end
