bcfdcfe9c5868bd07af2244d429480164dd2a196
kpolaiah <kpolaiah@heresy.in>
2013-01-04 16:46:09 +0530

    Feed show page changes in admin.

--- app/admin/feeds.rb
+++ app/admin/feeds.rb
@@ -15,11 +15,29 @@ ActiveAdmin.register Feed do
   end
 
   show do
-    attributes_table :feed,:public
-    panel "feed_types" do
-      attributes_table_for feed.feed_type do
-     	row("Post Type") {feed.feed_type.post_type}
+    panel "Feed details" do
+     attributes_table_for  feed do
+      row("Feed") {feed.feed}
+      row("public") {feed.public}
+      row("Post Type") {feed.feed_type.post_type}  
+      case feed.feed_type.post_type
+       when "Photos"      
+          row("Feed Image") {feed.feed_image.nil? ? "" : (image_tag feed.feed_image.feedimage) }
+      when "Videos"
+          row("Feed Video") {begin OEmbed::Providers.get(feed.feed_hash["video"], {:maxwidth => 355, :maxheight => 300}).html.html_safe rescue "Video not found or removed at source." end }
+       when "Events"
+        row("Event Image") {feed.event_image.nil? ? "" : (image_tag feed.event_images)}
+        row("Event") {feed.feed_hash["event_name"]}
+        row("Details") {feed.feed_hash["feed.feed"]}
+        row("Event time") {feed.feed_hash["event_date"] + feed.feed_hash["event_time"]}
+        row("Location") {feed.feed_hash["event_location"]}
+       when "Deals"
+        row("Deal Image") {feed.deal_image.nil? ? "" : (image_tag feed.deal_images)}
+        row("Sale Price") {"$" + feed.feed_hash["deal_sale_price"]}
+        row("Sale Link") {feed.feed_hash["deal_sale_link"]}
+        row("Deal Date") {"expires: "+feed.feed_hash["deal_date"]}
       end
+     end
     end
     panel "User Feeds" do
       unless feed.user_feeds.empty?
