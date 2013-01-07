35f3a26bcfc13ce7882e76312f0263c386a35848
kpolaiah <kpolaiah@heresy.in>
2013-01-07 18:03:38 +0530

    query optimization in dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,9 +4,6 @@ ActiveAdmin.register_page "Dashboard" do
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
 
-   bod = Time.now.beginning_of_day.utc
-   eod = Time.now.end_of_day.utc
-
    columns do
      column do
        panel "Stats" do
@@ -14,32 +11,32 @@ ActiveAdmin.register_page "Dashboard" do
            "Number of members:  #{User.count}"
          end
          div do
-           "Number of signups today: #{User.where(:created_at.gte => bod,:created_at.lte => eod).count}"
+           "Number of signups today: #{User.where(:created_at => Time.now.all_day).count}"
          end
          div do
-           "Number of signups this week: #{User.where(:created_at.gte => Time.now.beginning_of_week.utc,:created_at.lte => Time.now.end_of_week.utc ).count}"
+           "Number of signups this week: #{User.where(:created_at => Time.now.all_week).count}"
          end
          div do
-           "Number of signups this month: #{User.where(:created_at.gte => Time.now.beginning_of_month.utc,:created_at.lte => Time.now.end_of_month.utc ).count}"
+           "Number of signups this month: #{User.where(:created_at => Time.now.all_month).count}"
          end
          div do
            div do
-            "Number of posts today: #{Feed.where(:created_at.gte => bod,:created_at.lte => eod).count}"
+            "Number of posts today: #{Feed.where(:created_at => Time.now.all_day).count}"
            end
            div do
-             "Updates: #{Feed.where(:created_at.gte => bod,:created_at.lte => eod, :feed_type_id => FeedType.where(:post_type => 'Update').first.id).count}"
+             "Updates: #{Feed.where(:created_at  => Time.now.all_day, :feed_type_id => FeedType.where(:post_type => 'Update').first.id).count}"
            end
            div do
-             "Photos: #{Feed.where(:created_at.gte => bod,:created_at.lte => eod, :feed_type_id => FeedType.where(:post_type => 'Photos').first.id).count}"
+             "Photos: #{Feed.where(:created_at => Time.now.all_day, :feed_type_id => FeedType.where(:post_type => 'Photos').first.id).count}"
            end
            div do
-             "Videos: #{Feed.where(:created_at.gte => bod,:created_at.lte => eod, :feed_type_id => FeedType.where(:post_type => 'Videos').first.id).count}"
+             "Videos: #{Feed.where(:created_at => Time.now.all_day, :feed_type_id => FeedType.where(:post_type => 'Videos').first.id).count}"
            end
            div do
-             "Events: #{Feed.where(:created_at.gte => bod,:created_at.lte => eod, :feed_type_id => FeedType.where(:post_type => 'Events').first.id).count}"
+             "Events: #{Feed.where(:created_at => Time.now.all_day, :feed_type_id => FeedType.where(:post_type => 'Events').first.id).count}"
            end
            div do
-             "Deals: #{Feed.where(:created_at.gte => bod,:created_at.lte => eod, :feed_type_id => FeedType.where(:post_type => 'Deals').first.id).count}"
+             "Deals: #{Feed.where(:created_at => Time.now.all_day, :feed_type_id => FeedType.where(:post_type => 'Deals').first.id).count}"
            end
          end
          div do
