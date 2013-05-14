d95f6bd88ab168f7a727df494efe803722feeb42
ramya <ramya@heresy.in>
2013-05-14 12:38:13 +0530

    comment emotions modified.

--- app/admin/emotions.rb
+++ app/admin/emotions.rb
@@ -5,6 +5,7 @@ ActiveAdmin.register Emotion do
   index do
     column :name
     column :emotion_type
+    column :emotion_image
     column "Actions" do |emotion|
       span link_to "View", admin_emotion_path(emotion)
     end
@@ -13,7 +14,8 @@ ActiveAdmin.register Emotion do
   form do |f|                         
     f.inputs "Emotion Details" do       
       f.input :emotion_type                  
-      f.input :name               
+      f.input :name  
+      f.input :emotion_image            
       f.input :order 
     end                               
     f.actions                         
--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -5,6 +5,7 @@ class Emotion
   has_many :user_feeds
 
   field :name, :type => String
+  field :emotion_image
   field :order, :type => Integer
 
   validates_uniqueness_of :name
